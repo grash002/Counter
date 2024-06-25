952b3d372eb36dca882921a6238e4bbb4f07297a
Grash <Grash002@yandex.ru>
2024-06-25 13:15:27 +0300

    Поправил отступы и порядок перечисления в классе функций, параметров, аннотаций

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -11,12 +11,10 @@ import UIKit
 
 class ViewController: UIViewController {
     var counter:Int = 0
-    
     private func getCurrentDateTime () -> String{
         let currentDate = Date()
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        
         if let timeZoneAbbreviation = TimeZone.abbreviationDictionary["MSK"] {
             dateFormatter.timeZone = TimeZone(abbreviation: timeZoneAbbreviation)
         }
@@ -24,17 +22,13 @@ class ViewController: UIViewController {
         let currentDateInFormat = dateFormatter.string(from: currentDate)
         return currentDateInFormat
     }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         textView.text = "История изменений:"
         textView.clipsToBounds = true;
         textView.layer.cornerRadius = 10;
         labelCounter.text = "0"
-        
     }
-    
     @IBAction private func buttonMinusDidTap(_ sender: Any) {
         if counter - 1 >= 0 {
             let currentDateTime = getCurrentDateTime()
@@ -47,12 +41,6 @@ class ViewController: UIViewController {
             textView.text += "\n[\(currentDateTime)]: попытка уменьшить значение счетчика ниже 0"
         }
     }
-    
-    @IBOutlet private weak var labelCounter: UILabel!
-    
-    
-    @IBOutlet private weak var textView: UITextView!
-    
     @IBAction private func buttonRefreshDidTap(_ sender: Any) {
         counter = 0
         labelCounter.text = counter.codingKey.stringValue
@@ -67,6 +55,7 @@ class ViewController: UIViewController {
         print("The plus button was pressed. Current counter: \(counter)")
         textView.text += "\n[\(currentDateTime)]: значение изменено на +1"
     }
-    
+    @IBOutlet private weak var labelCounter: UILabel!
+    @IBOutlet private weak var textView: UITextView!
 }
 
